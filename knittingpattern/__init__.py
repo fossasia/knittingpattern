9c6a6b0f7efd17a24963c39bbcf75b4406054381
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-05-27 23:29:45 +0200

    pep8

--- knittingpattern/__init__.py
+++ knittingpattern/__init__.py
@@ -3,6 +3,7 @@ import json
 
 __version__ = '0.0.1'
 
+
 class KnittingPattern(object):
 
     def __init__(self, pattern):
@@ -27,17 +28,14 @@ class KnittingPattern(object):
     @classmethod
     def load_from_file(cls, file):
         return cls.load_from_object(json.load(file))
-    
+
     @classmethod
     def load_from_path(cls, path):
         with open(path) as file:
             return cls.load_from_file(file)
-    
+
 
 load_from_object = KnittingPattern.load_from_object
 load_from_string = KnittingPattern.load_from_string
 load_from_file = KnittingPattern.load_from_file
 load_from_path = KnittingPattern.load_from_path
-
-
-
