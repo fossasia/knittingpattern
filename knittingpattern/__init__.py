c9a19a1371535fa87fb6826bbdfe829e340d7892
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-05-27 23:27:54 +0200

    loading tests are passing

--- knittingpattern/__init__.py
+++ knittingpattern/__init__.py
@@ -1,1 +1,43 @@
+import json
+
+
 __version__ = '0.0.1'
+
+class KnittingPattern(object):
+
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    @property
+    def version(self):
+        return self.pattern["version"]
+
+    @property
+    def type(self):
+        return self.pattern["type"]
+
+    @classmethod
+    def load_from_object(cls, obj):
+        return cls(obj)
+
+    @classmethod
+    def load_from_string(cls, json_string):
+        return cls.load_from_object(json.loads(json_string))
+
+    @classmethod
+    def load_from_file(cls, file):
+        return cls.load_from_object(json.load(file))
+    
+    @classmethod
+    def load_from_path(cls, path):
+        with open(path) as file:
+            return cls.load_from_file(file)
+    
+
+load_from_object = KnittingPattern.load_from_object
+load_from_string = KnittingPattern.load_from_string
+load_from_file = KnittingPattern.load_from_file
+load_from_path = KnittingPattern.load_from_path
+
+
+
---
+++ knittingpattern/tests/test_parsing.py
@@ -0,0 +1,35 @@
+import knittingpattern
+from pytest import fixture
+import json
+import io
+
+EMPTY_PATTERN = {
+        "version" : "0.1",
+        "type" : "knitting pattern"
+    }
+
+def assert_is_pattern(pattern):
+    assert pattern.type == "knitting pattern"
+    assert pattern.version == "0.1"
+    
+def test_can_import_empty_pattern_from_object():
+    pattern = knittingpattern.load_from_object(EMPTY_PATTERN)
+    assert_is_pattern(pattern)
+
+def test_can_import_empty_pattern_from_string():
+    json_string = json.dumps(EMPTY_PATTERN)
+    pattern = knittingpattern.load_from_string(json_string)
+    assert_is_pattern(pattern)
+
+def test_can_import_empty_pattern_from_file_object():
+    json_string = json.dumps(EMPTY_PATTERN)
+    file = io.StringIO(json_string)
+    pattern = knittingpattern.load_from_file(file)
+    assert_is_pattern(pattern)
+
+def test_can_import_empty_pattern_from_path(tmpdir):
+    p = tmpdir.mkdir("sub").join("empty_pattern.knit")
+    with open(p.strpath, "w") as f:
+        json.dump(EMPTY_PATTERN, f)
+    pattern = knittingpattern.load_from_path(p.strpath)
+    assert_is_pattern(pattern)
