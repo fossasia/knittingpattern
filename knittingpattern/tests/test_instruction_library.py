7ecb829a3641e1991bf3ab63e33c295eae40f342
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-05-28 22:34:31 +0200

    added library tests

---
+++ knittingpattern/tests/test_instruction_library.py
@@ -0,0 +1,66 @@
+from pytest import fixture
+from knittingpattern.InstructionLibrary import InstructionLibrary
+from knittingpattern.KnittingContext import KnittingContext
+
+DESCRIPTION = "here you can see how to knit: URL"
+
+test_instructions = [
+        {
+            "type": "knit",
+            "description": DESCRIPTION
+        }, 
+        {
+            "type": "purl",
+            "inverse": "knit"
+        },
+        {
+            "type": "extravagant knit",
+            "color": "green",
+            "specialattribute": True
+        }
+    ]
+
+
+@fixture
+def library(knitting_context):
+    return InstructionLibrary(knitting_context).object(test_instructions)
+
+
+@fixture
+def knit(library):
+    return library.as_instruction({"type": "knit"})
+
+
+@fixture
+def purl(library):
+    return library.as_instruction({"type": "purl", "color": "white"})
+
+
+@fixture
+def custom_knit(library):
+    return library.as_instruction({"type": "extravagant knit"})
+
+
+def test_knit_type_attributes(knit):
+    assert knit.type == "knit"
+    assert knit["description"] == DESCRIPTION
+    assert knit["type"] == knit.type
+
+
+def test_knit_has_no_color(knit):
+    assert "color" not in knit
+    assert "type" in knit
+
+
+def test_purl_has_color(purl):
+    assert purl.color == "white"
+    assert "color" in purl
+
+
+def not_everyting_is_known_by_purl(purl)
+    assert "asd" not in purl
+    assert purl["inverse"] == "knit"
+
+
+def test_custom_type(custom_knit):
+    assert custom_type["specialattribute"] == True
