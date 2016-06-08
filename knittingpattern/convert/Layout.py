972f6dfbeb1bc32fcd105e3989c922bcf95bdf44
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-08 12:28:46 +0200

    instruction placing tests work

--- knittingpattern/convert/Layout.py
+++ knittingpattern/convert/Layout.py
@@ -1,33 +1,64 @@
-import itertools
+from itertools import chain
 
 class InstructionInGrid(object):
-    def __init__(self, instruction, x, y):
+    def __init__(self, instruction, x, y, subtract_width=False):
         self.instruction = instruction
         self.x = x
         self.y = y
         self.width = instruction.number_of_consumed_meshes
         self.height = 1
+        if subtract_width:
+            self.x -= self.width
 
+def identity(object):
+    return object
+    
+
+class RecursiveWalk(object):
+    def __init__(self, first_instruction):
+        self.first_instruction = first_instruction
+        self.instructions_in_grid = {}
+        self.todo = []
+        self.expand(first_instruction, 0, 0)
+        self.walk()
+        
+    def expand(self, *args, **kw):
+        self.todo.append((args, kw))
+        
+    def _walk(self, instruction, x, y, subtract_width=False, row = 0):
+        if instruction is None: return
+        if instruction in self.instructions_in_grid: return
+        print("{}{} at {},{} {}".format("  " * row, instruction, x, y, subtract_width))
+        in_grid = InstructionInGrid(instruction, x, y, subtract_width=subtract_width)
+        self.instructions_in_grid[instruction] = in_grid
+        self.expand(instruction.previous_instruction_in_row, x, y, subtract_width= True, row= row)
+        self.expand(instruction.next_instruction_in_row, x + in_grid.width, y, row= row)
+        for mesh in instruction.produced_meshes:
+            if not mesh.is_consumed(): continue
+            self.expand(mesh.consuming_instruction, x, y + in_grid.height, row= row + 1)
+                       
+    def walk(self):
+        while self.todo:
+            args, kw = self.todo.pop(0)
+            self._walk(*args, **kw)
+
+    def in_grid(self, instruction):
+        return self.instructions_in_grid[instruction]
         
 
 class GridLayout(object):
     def __init__(self, pattern):
         self.pattern = pattern
+        self._rows = list(sorted(self.pattern.rows))
+        self._walk = RecursiveWalk(self._rows[0].instructions[0])
         
-    def walk_instructions(self, mapping):
-        instruction_grid =  list(self.walk_rows(lambda kack: kack.instructions))
-        instruction_objects = []
-        for y, row_instructions in enumerate(instruction_grid):
-            x = 0
-            for instruction in row_instructions:
-                instruction_object = InstructionInGrid(instruction, x, y)
-                instruction_objects.append(instruction_object)
-                x += instruction_object.width
-                assert instruction_object.height == 1
-        return list(map(mapping, instruction_objects))
+    def walk_instructions(self, mapping=identity):
+        instructions = chain(*self.walk_rows(lambda row: row.instructions))
+        grid = map(self._walk.in_grid, instructions)
+        return map(mapping, grid)
         
-    def walk_rows(self, mapping):
-        return list(map(mapping, sorted(self.pattern.rows)))
+    def walk_rows(self, mapping=identity):
+        return map(mapping, self._rows)
         
-    def walk_connections(self, mapping):
+    def walk_connections(self, mapping=identity):
         return []
--- knittingpattern/convert/test/test_layout.py
+++ knittingpattern/convert/test/test_layout.py
@@ -5,24 +5,24 @@ from knittingpattern import load_from_relative_file
 
 
 def coordinates(layout):
-    return layout.walk_instructions(lambda point: (point.x, point.y))
+    return list(layout.walk_instructions(lambda point: (point.x, point.y)))
 
 
 def sizes(layout):
-    return layout.walk_instructions(lambda point: (point.width, point.height))
+    return list(layout.walk_instructions(lambda point: (point.width, point.height)))
 
 
 def instructions(layout):
-    return layout.walk_instructions(lambda point: point.instruction)
+    return list(layout.walk_instructions(lambda point: point.instruction))
 
 
 def row_ids(layout):
-    return layout.walk_rows(lambda row: row.id)
+    return list(layout.walk_rows(lambda row: row.id))
 
 
 def connections(layout):
-    return layout.walk_connections(lambda c: (c.start_point.xy,
-                                              c.end_point.xy))
+    return list(layout.walk_connections(lambda c: (c.start_point.xy,
+                                              c.end_point.xy)))
 
 
 class BaseTest:
@@ -86,8 +86,155 @@ class TestAddAndRemoveMeshes(BaseTest):
             (0, 2), (1, 2), (2, 2),
             (-1, 3), (0, 3), (1, 3), (2, 3), (3, 3)
         ]
-    
 
+    # test how instructions are connected
+        
+    @fixture
+    def i1(self, pattern):
+        return pattern.rows[1].instructions
+        
+    @fixture
+    def i2(self, pattern):
+        return pattern.rows[2].instructions
+        
+    @fixture
+    def i3(self, pattern):
+        return pattern.rows[3].instructions
+        
+    @fixture
+    def i4(self, pattern):
+        return pattern.rows[4].instructions
+        
+    @fixture
+    def instructions(self, i1, i2, i3, i4):
+        return i1 + i2 + i3 + i4
+        
+    def test_all_consume_one_mesh(self, instructions):
+        assert all(i.number_of_consumed_meshes == 1
+                   for i in instructions)
+                   
+    def test_all_produce_one_mesh(self, instructions):
+        assert all(i.number_of_produced_meshes == 1
+                   for i in instructions)
+           
+    # i1 produced
+           
+    def test_i1_0_is_not_produced(self, i1):
+        assert i1[0].producing_instructions == [None]
+    
+    def test_i1_1_is_not_produced(self, i1):
+        assert i1[1].producing_instructions == [None]
+    
+    def test_i1_2_is_not_produced(self, i1):
+        assert i1[2].producing_instructions == [None]
+
+    def test_i1_3_is_not_produced(self, i1):
+        assert i1[3].producing_instructions == [None]
+        
+    # i1 consumed
+        
+    def test_i1_0_consumed(self, i1, i2):
+        assert i1[0].consuming_instructions == [i2[0]]
+        
+    def test_i1_1_consumed(self, i1, i2):
+        assert i1[1].consuming_instructions == [i2[1]]
+        
+    def test_i1_2_consumed(self, i1, i2):
+        assert i1[2].consuming_instructions == [i2[2]]
+        
+    def test_i1_3_consumed(self, i1, i2):
+        assert i1[3].consuming_instructions == [i2[3]]
+
+    # i2 produced
+    
+    def test_i2_0_produced(self, i1, i2):
+        assert i2[0].producing_instructions == [i1[0]]
+
+    def test_i2_1_produced(self, i1, i2):
+        assert i2[1].producing_instructions == [i1[1]]
+
+    def test_i2_2_produced(self, i1, i2):
+        assert i2[2].producing_instructions == [i1[2]]
+
+    def test_i2_3_produced(self, i1, i2):
+        assert i2[3].producing_instructions == [i1[3]]
+
+    def test_i2_4_produced(self, i2):
+        assert i2[4].producing_instructions == [None]
+
+    # i2 consumed
+        
+    def test_i2_0_consumed(self, i2, i3):
+        assert i2[0].consuming_instructions == [i3[0]]
+        
+    def test_i2_1_consumed(self, i2, i3):
+        assert i2[1].consuming_instructions == [i3[1]]
+        
+    def test_i2_2_consumed(self, i2, i3):
+        assert i2[2].consuming_instructions == [i3[2]]
+        
+    def test_i2_3_not_consumed(self, i2):
+        assert i2[3].consuming_instructions == [None]
+        
+    def test_i2_4_not_consumed(self, i2):
+        assert i2[4].consuming_instructions == [None]
+
+    # i3 produced
+        
+    def test_i3_0_produced(self, i2, i3):
+        assert i3[0].producing_instructions == [i2[0]]
+        
+    def test_i3_1_produced(self, i2, i3):
+        assert i3[1].producing_instructions == [i2[1]]
+        
+    def test_i3_2_produced(self, i2, i3):
+        assert i3[2].producing_instructions == [i2[2]]
+
+    # i3 consumed
+        
+    def test_i3_0_consumed(self, i3, i4):
+        assert i3[0].consuming_instructions == [i4[1]]
+        
+    def test_i3_1_consumed(self, i3, i4):
+        assert i3[1].consuming_instructions == [i4[2]]
+        
+    def test_i3_2_consumed(self, i3, i4):
+        assert i3[2].consuming_instructions == [i4[3]]        
+
+    # i4 produced
+    
+    def test_i4_0_not_produced(self, i4):
+        assert i4[0].producing_instructions == [None]
+    
+    def test_i4_1_produced(self, i3, i4):
+        assert i4[1].producing_instructions == [i3[0]]
+    
+    def test_i4_2_produced(self, i3, i4):
+        assert i4[2].producing_instructions == [i3[1]]
+    
+    def test_i4_3_produced(self, i3, i4):
+        assert i4[3].producing_instructions == [i3[2]]
+    
+    def test_i4_4_not_produced(self, i4):
+        assert i4[4].producing_instructions == [None]
+
+    # i4 consumed
+    
+    def test_i4_0_not_consumed(self, i4):
+        assert i4[0].consuming_instructions == [None]
+    
+    def test_i4_1_not_consumed(self, i4):
+        assert i4[1].consuming_instructions == [None]
+    
+    def test_i4_2_not_consumed(self, i4):
+        assert i4[2].consuming_instructions == [None]
+    
+    def test_i4_3_not_consumed(self, i4):
+        assert i4[3].consuming_instructions == [None]
+    
+    def test_i4_4_not_consumed(self, i4):
+        assert i4[4].consuming_instructions == [None]
+        
 class _TestSplitUpMeshes(BaseTest):
     FILE = "split_up_and_add_rows.json"
     SIZES = [(1, 1)] * 17
@@ -103,5 +250,12 @@ class _TestSplitUpMeshes(BaseTest):
     LARGER_CONNECTIONS = [((0, 1), (0, 3)), ((1, 1), (1, 3)), ((2, 0), (2, 3))]
 
 
+def test_use_row_with_lowest_number_of_incoming_connections_as_first_row():
+    fail()
 
+def test_if_row_with_lowest_number_of_connections_exist_use_smallest_id():
+    fail()
 
+    
+test_use_row_with_lowest_number_of_incoming_connections_as_first_row = "TODO"
+test_if_row_with_lowest_number_of_connections_exist_use_smallest_id = "TODO"
