5afb40ce6d7fb6ebe367a12a26abe8b422366f6e
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-10 20:23:04 +0200

    tested dumper

--- knittingpattern/test/test_dumper.py
+++ knittingpattern/test/test_dumper.py
@@ -1,117 +1,85 @@
 from test import *
 from knittingpattern.Dumper import ContentDumper
-import io
+from io import StringIO
 from tempfile import mktemp
+import os
 
 
 STRING = "asdf1234567890"
 
 
 @fixture
-def string():
-    def string(dump):
-        dump.string(STRING[:1])
-        dump.string(STRING[1:])
-    return ContentDumper(string)
+def save_to():
+    def dump_my_data_structure(file):
+        file.write(STRING[:1])
+        file.write(STRING[1:])
+    return ContentDumper(dump_my_data_structure)
 
 
 @fixture
-def file():
-    def file(dump):
-        file = dump.file()
-        file.write(STRING)
-    return ContentDumper(file)
+def temp_file(save_to):
+    return save_to.temporary_file()
 
 
 @fixture
-def temporary_path():
-    def temporary_path(dump):
-        temporary_path = dump.get_path()
-        with open(temporary_path, "w") as file:
-            file.write(STRING)
-    return ContentDumper(temporary_path)
+def stringio():
+    return StringIO()
 
 
-@fixture
-def path():
-    def path(dump):
-        path = mktemp()
-        with open(temporary_path, "w") as file:
-            file.write(STRING)
-        dump.path(path)
-    return ContentDumper(path)
+def assert_string_is_file_content(file):
+    file.seek(0)
+    assert file.read() == STRING
 
 
-def pytest_generate_tests(metafunc):
-    if "dumper" in metafunc.fixturenames:
-        metafunc.parametrize("dumper", [string(), file(), temporary_path(), 
-                                        path()])
+def assert_string_is_path_content(path):
+    with open(path) as file:
+        assert file.read() == STRING   
 
 
 def test_string_is_long():
     assert len(STRING) > 5
 
 
-def test_dump_to_string(dumper):
-    assert dumper.string() == STRING
+def test_dump_to_string(save_to):
+    assert save_to.string() == STRING
 
 
-def test_dump_to_file(dumper):
-    file = io.StringIO()
-    dumper.file(file)
-    file.seek(0)
-    assert file.read() == STRING
+def test_dump_to_file(save_to, stringio):
+    save_to.file(stringio)
+    stringio.seek(0)
+    assert_string_is_file_content(stringio)
 
 
-def test_dump_to_path(dumper, tmpdir):
+def test_dump_is_behind_content_in_file(save_to, stringio):
+    save_to.file(stringio)
+    assert stringio.read() == ""
+    
+
+def test_dump_to_path(save_to, tmpdir):
     path = tmpdir.mkdir("sub").join("temp.txt").strpath
-    dumper.path(path)
-    with open(path) as f:
-        assert f.read() == STRING
+    save_to.path(path)
+    assert_string_is_path_content(path)
 
 
-def test_dump_to_temp_file(dumper):
-    path = dumper.temporary_path()
-    with open(path) as file:
-        assert file.read() == STRING
+def test_dump_to_temp_path(save_to):
+    path = save_to.temporary_path()
+    assert_string_is_path_content(path)
 
 
-def test_string_is_wanted():
-    @ContentDumper
-    def string(dump):
-        assert dump.prefers_string()
-        assert not dump.prefers_file()
-        assert not dump.prefers_path()
-        assert not dump.prefers_get_path()
-    string.string()
+def test_dump_to_temporary_file(temp_file):
+    assert_string_is_file_content(temp_file)
 
 
-def test_file_is_wanted():
-    @ContentDumper
-    def file(dump):
-        assert not dump.prefers_string()
-        assert dump.prefers_file()
-        assert not dump.prefers_path()
-        assert not dump.prefers_get_path()
-    file.file()
+def test_temporary_file_is_deleted_on_default(temp_file):
+    temp_file.close()
+    assert not os.path.isfile(temp_file.name)
 
 
-def test_fixed_path_is_wanted(tmpdir):
-    p = tmpdir.join("test.txt")
-    @ContentDumper
-    def path(dump):
-        assert not dump.prefers_string()
-        assert not dump.prefers_file()
-        assert not dump.prefers_path()
-        assert dump.prefers_get_path()
-    path.path(p)
+def test_temporary_file_exists(temp_file):
+    assert os.path.isfile(temp_file.name)
 
 
-def test_path_is_wanted():
-    @ContentDumper
-    def path(dump):
-        assert not dump.prefers_string()
-        assert not dump.prefers_file()
-        assert dump.prefers_path()
-        assert dump.prefers_get_path()
-    path.path()
+def test_temporary_file_has_option_for_deletion(save_to):
+    file = save_to.temporary_file(delete_when_closed=False)
+    file.close()
+    assert_string_is_path_content(file.name)
