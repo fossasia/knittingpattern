3784728caf65cd153f3fe2414f6d15b3a73db567
Nicco Kunzmann <niccokunzmann@googlemail.com>
2016-06-16 15:33:59 +0200

    removed imports from __init__.py

--- knittingpattern/__init__.py
+++ knittingpattern/__init__.py
@@ -1,18 +1,47 @@
-from .KnittingContext import KnittingContext
+# there should be no imports
 
 __version__ = '0.0.4a'
 
-knitting_context = KnittingContext()
+def _load()
+    """Returns the loader of a KnittingContext to load objects from."""
+    from .KnittingContext import KnittingContext
+    return KnittingContext.load
 
-load_from_object = knitting_context.load.object
-load_from_string = knitting_context.load.string
-load_from_file = knitting_context.load.file
-load_from_path = knitting_context.load.path
-load_from_url = knitting_context.load.url
-load_from_relative_file = knitting_context.load.relative_file
+
+def load_from_object(object):
+    """Load a knitting pattern from an object."""
+    return _load().object(object)
+
+
+def load_from_string(string):
+    """Load a knitting pattern from a string."""
+    return _load().string(string)
+
+
+def load_from_file(file):
+    """Load a knitting pattern from a file-like object."""
+    return _load().file(file)
+
+
+def load_from_path(path):
+    """Load a knitting pattern from a file behind located at `path`."""
+    return _load().path(path)
+
+
+def load_from_url(url):
+    """Load a knitting pattern from a url."""
+    return _load().url(url)
+
+
+def load_from_relative_file(module, path_relative_to):
+    """Load a knitting pattern from a `path_relative_to` a `module`.
+    
+    `module` can be a module, a module's name or a module's path."""
+    return _load().relative_file(module, path_relative_to)
+
 
 __all__ = [
-        "KnittingContext", "knitting_context", "load_from_object",
+        "load_from_object",
         "load_from_string", "load_from_file", "load_from_path",
         "load_from_url", "load_from_relative_file"
     ]
