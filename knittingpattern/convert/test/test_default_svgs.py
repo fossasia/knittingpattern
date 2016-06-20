5da5189de5df260da8169e835d5f0292020141c8
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-21 00:04:28 +0200

    fixed errors

--- knittingpattern/convert/test/test_default_svgs.py
+++ knittingpattern/convert/test/test_default_svgs.py
@@ -1,25 +1,26 @@
 from test import *
-from knittingpattern.convert.InstructionToSVG import default_instruction_to_svg
+from knittingpattern.convert.InstructionToSVG import \
+    default_instructions_to_SVG
 from collections import namedtuple
 
 Instruction = namedtuple("Instruction", ["type"])
 
 @fixture(scope="module")
 def default():
-    return default_instruction_to_svg()
+    return default_instructions_to_SVG()
     
 
 def test_default_instruction_is_not_the_same(default):
     """This allows loading different svgs based on the default set."""
-    assert default_instruction_to_svg() != default
+    assert default_instructions_to_SVG() != default
 
 
-default_files = os.path.listdir(os.path.join(HERE, "..", "instruction-svg"))
+default_files = os.listdir(os.path.join(HERE, "..", "..", "instructions"))
 default_types = [os.path.splitext(file)[0] for file in default_files]
 @pytest.mark.parametrize('instruction', list(map(Instruction, default_types)))
 def test_instructions_have_svg(default, instruction):
-    assert default.has_instruction(instruction)
+    assert default.has_svg_for_instruction(instruction)
 
 
 def test_default_does_not_have_all_instructions(default):
-    assert not default.has_instruction(Instruction("asjdkalks"))
+    assert not default.has_svg_for_instruction(Instruction("asjdkalks"))
