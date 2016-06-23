ebbb6646119a92e2b69ecc0dc487884134a9209d
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-23 23:54:03 +0200

    pzthon 3 intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -440,3 +440,4 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/3/': None}
