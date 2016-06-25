e4b1fbfb51dda139accf58c4bf94c5d928c12747
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-25 16:16:15 +0200

    using sphinx bootstrap theme

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_bootstrap_theme
+html_theme = "bootstrap"
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
