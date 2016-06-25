d3ffbb6b88edce8237445adc3fac6bd10b715453
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-26 00:37:55 +0200

    improved test_documentation_sources_exist.py
    https://landscape.io/github/AllYarnsAreBeautiful/knittingpattern/31/modules/docs/test/test_documentation_sources_exist.py#L11

--- docs/test/test_documentation_sources_exist.py
+++ docs/test/test_documentation_sources_exist.py
@@ -8,7 +8,6 @@ Run this module to create the missing documentation files.
 from test_docs import PACKAGE_LOCATION, PACKAGE, PACKAGE_DOCUMENTATION, \
     PACKAGE_ROOT
 import pytest
-from pytest import fixture
 from collections import namedtuple
 import os
 
@@ -42,20 +41,20 @@ def module_name_and_doc(relative_path):
 
 Module = namedtuple("Module", ["absolute_path", "path", "name", "doc_file",
                                "lines", "title"])
-modules = []
+MODULES = []
 
 
 def add_module(absolute_path):
     relative_path = relative_module_path(absolute_path)
     name, doc_path = module_name_and_doc(relative_path)
     if os.path.isfile(doc_path):
-        with open(doc_path) as f:
-            lines = f.readlines()
+        with open(doc_path) as file:
+            lines = file.readlines()
     else:
         lines = []
     relative_name = name.rsplit(".", 1)[-1]
     title = ":py:mod:`{}` Module".format(relative_name)
-    modules.append(Module(absolute_path, relative_path, name, doc_path, lines,
+    MODULES.append(Module(absolute_path, relative_path, name, doc_path, lines,
                           title))
 
 
@@ -72,18 +71,18 @@ CREATE_MODULE_MESSAGE = "You can execute {} to create the missing "\
                         "documentation file.".format(__file__)
 
 
-@pytest.mark.parametrize('module', modules)
+@pytest.mark.parametrize('module', MODULES)
 def test_module_has_a_documentation_file(module):
     assert os.path.isfile(module.doc_file), CREATE_MODULE_MESSAGE
 
 
-@pytest.mark.parametrize('module', modules)
+@pytest.mark.parametrize('module', MODULES)
 def test_documentation_references_module(module):
     # assert module.lines[0].strip() == ".. py:module:: " + module.name
     assert module.lines[1].strip() == ".. py:currentmodule:: " + module.name
 
 
-@pytest.mark.parametrize('module', modules)
+@pytest.mark.parametrize('module', MODULES)
 def test_documentation_has_proper_title(module):
     assert module.lines[2].strip() == ""
     assert module.lines[3].strip() == module.title
@@ -92,22 +91,23 @@ def test_documentation_has_proper_title(module):
 
 def create_new_module_documentation():
     """Create documentation so it fits the tests."""
-    for module in modules:
+    for module in MODULES:
         if not os.path.isfile(module.doc_file):
             directory = os.path.dirname(module.doc_file)
             os.makedirs(directory, exist_ok=True)
-            with open(module.doc_file, "w") as f:
-                f.write("\n")  # .. py:module:: " + module.name + "\n")
-                f.write(".. py:currentmodule:: " + module.name + "\n")
-                f.write("\n")
-                f.write(module.title + "\n")
-                f.write("=" * len(module.title) + "\n")
-                f.write("\n")
-                f.write(".. automodule:: " + module.name + "\n")
-                f.write("   :show-inheritance:\n")
-                f.write("   :members:\n")
-                f.write("   :special-members:\n")
-                f.write("\n")
+            with open(module.doc_file, "w") as file:
+                write = file.write
+                write("\n")  # .. py:module:: " + module.name + "\n")
+                write(".. py:currentmodule:: " + module.name + "\n")
+                write("\n")
+                write(module.title + "\n")
+                write("=" * len(module.title) + "\n")
+                write("\n")
+                write(".. automodule:: " + module.name + "\n")
+                write("   :show-inheritance:\n")
+                write("   :members:\n")
+                write("   :special-members:\n")
+                write("\n")
 
 
 create_new_module_documentation()
