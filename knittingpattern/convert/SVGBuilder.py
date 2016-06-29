da6868c1bc270e60337fe1b4dfa96651d078e302
Kirstin Heidler <kirstin.heidler@gmx.net>
2016-06-29 17:28:43 +0200

    documentation build passing without warning

--- knittingpattern/convert/SVGBuilder.py
+++ knittingpattern/convert/SVGBuilder.py
@@ -123,7 +123,7 @@ class SVGBuilder(object):
 
     def place_svg_use(self, symbol_id, layer_id, group=None):
         """Same as :meth:`place_svg_use_coords` but with implicit `x`  and `y`
-         which are set to `0` in this method and then
+        which are set to `0` in this method and then
         :meth:`place_svg_use_coords` is called."""
         self.place_svg_use_coords(0, 0, symbol_id, layer_id, group)
 
