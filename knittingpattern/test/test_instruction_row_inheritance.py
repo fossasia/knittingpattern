c69125002bef0ecd15c9a4e0a1b6a8836c2dd202
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-06-30 19:15:23 +0200

    pylint

--- knittingpattern/test/test_instruction_row_inheritance.py
+++ knittingpattern/test/test_instruction_row_inheritance.py
@@ -12,25 +12,31 @@ def coloring_pattern():
 
 @fixture
 def colored_row(coloring_pattern):
+    """The colored row."""
     return coloring_pattern.rows["colored"].instructions
 
 
 @fixture
 def uncolored_row(coloring_pattern):
+    """The uncolored row."""
     return coloring_pattern.rows["uncolored"].instructions
 
 
 def test_instruction_has_no_color(uncolored_row):
+    """Neither row nor instruction have a color."""
     assert uncolored_row[0].color is None
 
 
 def test_instruction_has_own_color(uncolored_row):
+    """Instruction uses own color."""
     assert uncolored_row[1].color == "yellow"
 
 
 def test_instruction_has_color_from_row(colored_row):
+    """No color is given in the instruction, the row color is used."""
     assert colored_row[1].color == "blue"
 
 
 def test_instruction_has_own_color_despite_row(colored_row):
+    """Instruction uses own color before row's color."""
     assert colored_row[0].color == "green"
