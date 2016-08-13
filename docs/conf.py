4116abc62eb7e717fd25631d20c04d8945926101
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-08-13 23:17:18 +0200

    pep8

--- docs/conf.py
+++ docs/conf.py
@@ -448,5 +448,5 @@ epub_exclude_files = ['search.html']
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     "https://docs.python.org/3/": None,
-    "http://observablelist.readthedocs.io/en/latest/" : None,
+    "http://observablelist.readthedocs.io/en/latest/": None,
     "https://kivy.org/docs/": None}
